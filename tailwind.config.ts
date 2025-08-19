52863ce89c94d65c8d3df523eaeede898831af83
Supratim Dhara <supratimdhara0@gmail.com>
2025-08-19 15:27:34 +0000

    use a classy font

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -17,8 +17,8 @@ export default {
     },
     extend: {
       fontFamily: {
-        body: ['"PT Sans"', 'sans-serif'],
-        headline: ['"Playfair Display"', 'serif'],
+        body: ['"Lato"', 'sans-serif'],
+        headline: ['"Cormorant Garamond"', 'serif'],
         code: ['monospace'],
       },
       colors: {
